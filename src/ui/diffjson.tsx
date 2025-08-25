a527a9c27853c8ce590e4cc59c2ce356d27d49f4
Cesar Martinez <cesarwamartinez@gmail.com>
2025-08-25 09:29:54 -0500

    fix layot

--- src/ui/diffjson.tsx
+++ src/ui/diffjson.tsx
@@ -3,6 +3,7 @@ import { diff } from 'jsondiffpatch';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { CodeEditorLazy, JsonViewerLazy } from '../components/lazy';
+import { JsonNode } from './formatter/formatter';
 
 export default function JsonDiffViewerModal() {
   const [json1, setJson1] = useState('');
@@ -39,57 +40,65 @@ export default function JsonDiffViewerModal() {
 
   return (
     <motion.div
-      initial={{ scale: 0.95, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      exit={{ scale: 0.95, opacity: 0 }}
-      transition={{ duration: 0.2 }}
-      className=" md:w-[90vw]  h-[100%] w-full bg-zinc-900 border border-zinc-800 rounded-2xl p-6 shadow-2xl text-sm text-zinc-200 "
+      className="max-w-7xl mx-auto h-full min-h-[90vh] flex flex-col p-6 rounded-2xl shadow-2xl text-sm bg-zinc-900 border border-zinc-800 text-zinc-200"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
     >
-      <h2 className="my-2 text-xl font-semibold  text-center bg-gradient-to-bl from-white to-zinc-600 bg-clip-text text-transparent">
+      <h2 className="text-center text-xl font-bold mb-4 bg-gradient-to-bl from-white to-zinc-600 bg-clip-text text-transparent">
         Comparador JSON
       </h2>
-      <div className="grid md:grid-cols-2 gap-4  ">
-        <div className="flex flex-col">
+      <div className="flex-1 grid md:grid-cols-2 gap-4">
+        <div className="flex flex-col h-full">
           <label className="mb-1 text-zinc-400 font-medium">JSON #1</label>
-
-          <CodeEditorLazy
-            minHeight="200px"
-            classNameContainer="bg-zinc-950 text-zinc-100  rounded-lg  resize-none focus:outline-none focus:ring-2 focus:ring-orange-500"
-            placeholder={error}
-            language="json"
-            value={json1}
-            onChange={(e) => handleSetJson(e, 1)}
-          />
+          <div className="flex-1 overflow-hidden">
+            <CodeEditorLazy
+              minHeight="200px"
+              classNameContainer="bg-zinc-950 text-zinc-100 rounded-lg h-full overflow-hidden resize-none focus:outline-none focus:ring-2 focus:ring-orange-500"
+              placeholder={error}
+              language="json"
+              value={json1}
+              onChange={(e) => handleSetJson(e, 1)}
+            />
+          </div>
         </div>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col h-full">
           <label className="mb-1 text-zinc-400 font-medium">JSON #2</label>
-
-          <CodeEditorLazy
-            minHeight="200px"
-            classNameContainer="bg-zinc-950 text-zinc-100  rounded-lg  resize-none focus:outline-none focus:ring-2 "
-            placeholder={error}
-            language="json"
-            value={json2}
-            onChange={(e) => handleSetJson(e, 2)}
-          />
+          <div className="flex-1 overflow-hidden">
+            <CodeEditorLazy
+              minHeight="200px"
+              classNameContainer="bg-zinc-950 text-zinc-100 rounded-lg h-full overflow-hidden resize-none focus:outline-none focus:ring-2"
+              placeholder={error}
+              language="json"
+              value={json2}
+              onChange={(e) => handleSetJson(e, 2)}
+            />
+          </div>
         </div>
       </div>
 
-      <div className="text-center ">
+      <div className="text-center my-4">
         <button
           onClick={handleCompare}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 my-5 rounded-lg shadow transition-colors"
+          className="bg-sky-500 hover:bg-sky-600 text-white font-semibold px-6 py-2 rounded-lg shadow-lg transition-colors"
         >
           Comparar
         </button>
       </div>
 
-      {diffResult === undefined && (
-        <p className="text-center p-4">No hay Cambios</p>
-      )}
+      {diffResult !== null && (
+        <div className="flex-1 overflow-y-auto p-4 rounded-lg bg-zinc-950">
+          <h3 className="text-xl font-semibold mb-2">Resultado 🎅</h3>
+          <div className='overflow-y-scroll max-h-[400px]'>
+          <JsonNode data={diffResult} __changed={diffResult} />
 
-      <JsonViewerLazy data={diffResult} __changed={diffResult} />
+          </div>
+        </div>
+      )}
+      {diffResult === undefined && (
+        <p className="text-center p-4 text-green-400">No hay Cambios</p>
+      )}
     </motion.div>
   );
 }
